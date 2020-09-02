1db4c2acef4c8dcd3e4daf6d223d373da21a20d2
Sudip Bhujel <devsudipbhujel@gmail.com>
2020-09-02 08:41:52 +0545

    adds Pillow package configs and image field to recipe model 🦾

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,7 +1,9 @@
+from unittest.mock import patch
+
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-from core.models import Ingredient, Recipe, Tag
+from core.models import Ingredient, Recipe, Tag, recipe_image_file_path
 
 
 def sample_user(email='test@recipeapp.com', password='test123'):
@@ -72,3 +74,13 @@ class ModelTest(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    @patch('uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test that image is saved in the correct location"""
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = recipe_image_file_path(None, 'myimage.jpg')
+
+        exp_path = f'uploads/recipe/{uuid}.jpg'
+        self.assertEqual(file_path, exp_path)
